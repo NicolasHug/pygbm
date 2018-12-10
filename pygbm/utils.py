144809ebe11d609d0e70059241c51f7c297d0192
Nicolas Hug <contact@nicolas-hug.com>
2018-12-10 08:08:34 -0500

    Addressed comments

--- pygbm/utils.py
+++ pygbm/utils.py
@@ -4,17 +4,20 @@ from lightgbm import LGBMClassifier
 from .gradient_boosting import GradientBoostingClassifier
 
 
-def get_lightgbm_estimator(pygbm_est):
-    """Return an estimator from LightGBM that has the same hyperparameters
-    as pygbm_est"""
+def get_lightgbm_estimator(pygbm_estimator):
+    """Return an unfitted LightGBM estimator with matching hyperparams.
 
-    pygbm_params = pygbm_est.get_params()
+    This utility function takes care of renaming the PyGBM parameters into
+    their LightGBM equivalent parameters.
+    """
+
+    pygbm_params = pygbm_estimator.get_params()
 
     if pygbm_params['loss'] == 'auto':
         raise ValueError('auto loss is not accepted. We need to know if '
                          'the problem is binary or multiclass classification.')
     if pygbm_params['scoring'] is not None:
-        raise ValueError('Early stopping should be deactivated.')
+        raise NotImplementedError('Early stopping should be deactivated.')
 
     loss_mapping = {
         'least_squares': 'regression_l2',
@@ -44,7 +47,9 @@ def get_lightgbm_estimator(pygbm_est):
         lgbm_params['min_sum_hessian_in_leaf'] *= 2
         lgbm_params['learning_rate'] *= 2
 
-    Est = (LGBMClassifier if isinstance(pygbm_est, GradientBoostingClassifier)
-           else LGBMRegressor)
+    if isinstance(pygbm_estimator, GradientBoostingClassifier):
+        Est = LGBMClassifier
+    else:
+        Est = LGBMRegressor
 
     return Est(**lgbm_params)
